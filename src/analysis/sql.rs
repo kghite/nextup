d005f7444506e2fbeb0ba9b8a99a28e63dab9aa1
kghite <kghite@gmail.com>
2023-08-13 12:01:23 +0200

    Test audit logging

--- src/analysis/sql.rs
+++ src/analysis/sql.rs
@@ -1,53 +1,30 @@
 use anyhow::{Context, Error, Result};
 use turbosql::{Turbosql, select, execute};
 
+trait dbLog{}
+
 #[derive(Turbosql, Default)]
-struct Project {
+struct ProjectLog {
     rowid: Option<i64>, // rowid member required & enforced at compile time
     slot: Option<String>,
     title: Option<String>,
     timestamp: Option<uszie>, // Convert to chrono
 }
 
+impl dbLog for ProjectLog{}
 
 #[derive(Turbosql, Default)]
-struct Audit {
+struct AuditLog {
     rowid: Option<i64>, // rowid member required & enforced at compile time
     timestamp: Option<uszie>, // Convert to chrono
     projectid: Option<i64>,
     nextup: Option<String>,
 }
 
-/// Initialize the sqllite db
-pub fn log_audit() -> Result<(), Error>  {
-    // EXAMPLE
-    let name = "Joe";
-    // INSERT a row
-    let rowid = Person {
-        name: Some(name.to_string()),
-        age: Some(42),
-        ..Default::default()
-    }.insert()?;
+impl dbLog for AuditLog{}
 
-    // SELECT all rows
-    let people = select!(Vec<Person>)?;
-
-    // SELECT multiple rows with a predicate
-    let people = select!(Vec<Person> "WHERE age > " 21)?;
-
-    // SELECT a single row with a predicate
-    let mut person = select!(Person "WHERE name = " name)?;
-
-    // UPDATE based on rowid, rewrites all fields in database row
-    person.age = Some(43);
-    person.update()?;
-
-    // UPDATE with manual SQL
-    execute!("UPDATE person SET age = " 44 " WHERE name = " name)?;
-
-    // DELETE
-    execute!("DELETE FROM person WHERE rowid = " 1)?;
-    // EXAMPLE
-
+/// Log nextup write actions for analysis
+pub fn log_write_action<Loggable:dbLog>(action: &mut Loggable) -> Result<(), Error>  {
+    let rowid = action.insert().with_context(|| format!("Could not save action to the db."))?;
     Ok(())
 }
